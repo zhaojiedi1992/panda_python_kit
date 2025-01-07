ec2dbf4e952b1afac7349d300bb62430e0205fd6
zhaojiedi <zhaojiedi@kuaishou.com>
2025-01-07 20:29:20 +0800

    add

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="panda-python-kit",
-    version="0.0.27",
+    version="0.0.28",
     author="zhaojiedi",
     author_email="1072892917@qq.com",
 
