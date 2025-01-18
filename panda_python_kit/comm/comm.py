b529d8b02fb9b3773b67bc369babd761b9ee97e6
zhaojiedi1992 <zhaojiedi1992@outlook.com>
2025-01-18 22:55:04 +0800

    Update comm.py

--- panda_python_kit/comm/comm.py
+++ panda_python_kit/comm/comm.py
@@ -8,9 +8,13 @@ def get_request_data(request):
     """根据请求类型获取请求参数"""
     if request.method == 'GET':
         # 对于GET请求, 获取URL的查询参数
+        if request.args is None:
+            return {}
         return request.args
     elif request.method == 'POST':
         # 对于POST请求，预期请求体为JSON
+        if request.get_json() is None:
+            return {}
         return request.get_json()
     else:
         # 可以扩展以支持其他HTTP方法
